5c5e7e75a0a88c65a300114fdd32034b26649c8f
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-05-09 11:13:43 -0400

    Cards added and styled. Remaining items broken into components.

--- solution/src/scripts/index.js
+++ solution/src/scripts/index.js
@@ -2,3 +2,13 @@ import 'reset-css';
 import '../main.scss';
 import '../components/header/header';
 import '../components/hero/hero';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: [
+      'Open Sans:400,700', 'sans-serif',
+      'Poppins:600', 'sans-serif'
+    ]
+  }
+});
