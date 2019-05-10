c5f36ac6f26053f32bd6735362170367802bdfa8
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-05-10 16:23:10 -0400

    mobile version done

--- solution/src/components/hero/hero.js
+++ solution/src/components/hero/hero.js
@@ -5,7 +5,7 @@ import '../hero/hero.scss';
 console.log('running hero')
 const heroContainer = document.querySelector('.hero__container');
 if (heroContainer.offsetWidth < 750) {
-  document.getElementById('hero-bg-image').style.background = `url('${bgHeroMobile}')`;
+  document.getElementById('hero-bg-image').style.backgroundImage = `url('${bgHeroMobile}')`;
 } else {
-  document.getElementById('hero-bg-image').style.background = `url('${bgHeroDesktop}')`;
+  document.getElementById('hero-bg-image').style.backgroundImage = `url('${bgHeroDesktop}')`;
 }
